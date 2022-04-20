a42db150303f5d84c3bf80220c3aefc677f5025c
moshabbir-dotcom <moshabbir@ymail.com>
2022-04-20 10:12:52 +0000

    Admin panel customised

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Product, Department
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Department)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Department, DepartmentAdmin)
