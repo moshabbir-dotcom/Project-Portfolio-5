319b703d7d863035371822efd5124b6fbda90027
moshabbir-dotcom <moshabbir@ymail.com>
2022-05-14 17:13:10 +0000

    Add blogpost app & models

--- blogpost/models.py
+++ blogpost/models.py
@@ -1,41 +1,41 @@
-# from django.db import models
-# from django.db.models.signals import pre_save
-# from django.utils.text import slugify
-# from django.dispatch import receiver
-# from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# # Create your models here
+# Create your models here
 
-# class BlogPost(models.Model):
-#     slug = models.SlugField(max_length=200, unique=True)
-#     title = models.CharField(max_length=20, unique=True)
-#     content = models.TextField()
-#     created_on = models.DateTimeField(auto_now_add=True)
-#     modified = models.DateTimeField(auto_now_add=True)
-#     author = models.ForeignKey(
-#         User, on_delete=models.CASCADE, related_name="blogpost", default=1)
+class BlogPost(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=20, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blogpost", default=1)
 
-#     class Meta:
-#         ordering = ['-created_on']
+    class Meta:
+        ordering = ['-created_on']
 
-#     def __str__(self):
-#         return self.title
-
-
-# def pre_save_blog_post_receiver(sender, instance, *args, **kwargs):
-#     if not instance.slug:
-#         instance.slug = slugify(
-#             instance.author.username + "-" + instance.title)
+    def __str__(self):
+        return self.title
 
 
-# pre_save.connect(pre_save_blog_post_receiver, sender=BlogPost)
+def pre_save_blog_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(
+            instance.author.username + "-" + instance.title)
 
 
-# # comments model
-# class Comment(models.Model):
-#     post = models.ForeignKey(
-#         BlogPost, related_name='comments', on_delete=models.CASCADE)
-#     comment_author = models.ForeignKey(
-#         User, on_delete=models.CASCADE, related_name="comment", default=1)
-#     comment = models.TextField()
-#     date_added = models.DateTimeField(auto_now_add=True)
+pre_save.connect(pre_save_blog_post_receiver, sender=BlogPost)
+
+
+# comments model
+class Comment(models.Model):
+    post = models.ForeignKey(
+        BlogPost, related_name='comments', on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comment", default=1)
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    # path('blogpost/', include('blogpost.urls')),
+    path('blogpost/', include('blogpost.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
