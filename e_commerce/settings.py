6c420d87bb9feb0f19e7439dacf912de9cfebc2a
moshabbir-dotcom <moshabbir@ymail.com>
2022-05-16 13:35:34 +0000

    Deploy to heroku via cli

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -122,20 +122,16 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-
-DATABASES = {
-        'default': dj_database_url.parse('postgres://rtwlorlbrobajm:361b01bf3d4c4b076241326a2b104be1da35eda49b13807d610267fbcb00f711@ec2-3-229-11-55.compute-1.amazonaws.com:5432/d7ep80bndi6omo')
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
 
 # Password validation
@@ -180,6 +176,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
