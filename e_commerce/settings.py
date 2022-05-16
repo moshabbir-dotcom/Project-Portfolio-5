7b669b1c2d7af4d0b666ed163c4db4417d81e269
moshabbir-dotcom <moshabbir@ymail.com>
2022-05-16 14:48:19 +0000

    Removed secret key & set debug

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f9$(e7e%!8!nkp8b#&!cs0s#y!vq2l&-hf4z*7@4r2a9=zr^t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aljild-walshier.herokuapp.com', 'localhost']
 
